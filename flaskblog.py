d5fdfa22c7fb8ad0ce36be85e83f94441f51aeae
youcefAZ <youcefdota@gmail.com>
2021-08-09 12:39:57 +0100

    added templates, learned html extend

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+games=[
+    {
+        'name': 'Half-life',
+        'devloper': 'Valve',
+        'year': '1998'
+    },
+    {
+        'name': 'The witcher 3',
+        'devloper': 'CD projekt RED',
+        'year': '2015'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, World!, whoops</h1>"
+    return render_template('home.html',games=games)
 
 @app.route("/about")
 def about():
-    return "<p>This is about noobs</p>"
+    return render_template('about.html',title='about')
 
 if __name__ == '__main__':
     app.run(debug=True)
